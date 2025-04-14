4f143ce2c679d5738bbb220fd6ded4a188e97b05
shameel-anjillath <55437857+shameel-anjillath@users.noreply.github.com>
2025-04-15 01:30:14 +0530

    V2.1.1.1

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/portfolio/' : '/',
+  base: '/portfolio/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
+    mode === 'production' &&
     componentTagger(),
   ].filter(Boolean),
   resolve: {
