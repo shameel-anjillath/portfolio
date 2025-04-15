00da8188d37ee45844b19e6370315fdc6c65c0c3
shameel-anjillath <55437857+shameel-anjillath@users.noreply.github.com>
2025-04-15 17:41:57 +0530

    V1

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,26 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { componentTagger } from 'lovable-tagger';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:'/portfolio/',
+  base: '/portfolio/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'production' &&
-    componentTagger(),
+    mode === 'production' && componentTagger(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: '.', // copy to dist/404.html later manually
+        }
+      ]
+    })
   ].filter(Boolean),
   resolve: {
     alias: {
